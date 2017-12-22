d6e6f342d9c4d519a90e318ccf3dc4d5a8989c5b
nanliyu <nancarpli@163.com>
2017-12-22 23:35:50 +0800

    smart-framework

---
+++ smartframework/src/main/java/org/smart4j/framework/Helper/BeanHelper.java
@@ -0,0 +1,43 @@
+package org.smart4j.framework.Helper;
+
+import org.smart4j.framework.util.ReflectionUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Bean 助手类
+ * Created by nanca on 12/22/2017.
+ */
+public final class BeanHelper {
+    /**
+     * 定义 Bean 映射（用于存放 Bean 类与 Bean 实例的映射关系）
+     */
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap<Class<?>, Object>();
+
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for (Class<?> beanClass : beanClassSet) {
+            Object obj = ReflectionUtil.newInstance(beanClass);
+            BEAN_MAP.put(beanClass, obj);
+        }
+    }
+
+    /**
+     * 获取 Bean 的映射
+     */
+    public static Map<Class<?>, Object> getBeanMap() {
+        return BEAN_MAP;
+    }
+
+    /**
+     * 获取 Bean 实例
+     */
+    public static <T> T getBean(Class<T> cls) {
+        if (!BEAN_MAP.containsKey(cls)) {
+            throw new RuntimeException("can not get bean by class: " + cls);
+        }
+        return (T) BEAN_MAP.get(cls);
+    }
+}
